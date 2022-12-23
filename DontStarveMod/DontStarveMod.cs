0c0a6e5ed24bc6b39f66e71f5bc748b30533f564
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2022-12-23 21:58:11 +0100

    Now you can starve

--- DontStarveMod/DontStarveMod.cs
+++ DontStarveMod/DontStarveMod.cs
@@ -1,15 +1,28 @@
-﻿using OWML.Common;
+﻿using System.Globalization;
+using System.Reflection;
+using HarmonyLib;
+using OWML.Common;
 using OWML.ModHelper;
+using UnityEngine;
 
 namespace DontStarveMod;
 
 public class DontStarveMod : ModBehaviour
 {
+    public static DontStarveMod Instance { get; private set; }
+
+    public const int HUNGER_TIME_START = 300;
+    public const int CRITICAL_THRESHOLD = 60;
+    
+    private static float _hungerTimeLeft = HUNGER_TIME_START;
+    private static bool _criticalThresholdMet = false;
+    private static bool _death = false;
+    private static OWScene _currentScene;
+    
     private void Awake()
     {
-        // You won't be able to access OWML's mod helper in Awake.
-        // So you probably don't want to do anything here.
-        // Use Start() instead.
+        Instance = this;
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
     }
 
     private void Start()
@@ -20,8 +33,49 @@ public class DontStarveMod : ModBehaviour
         // Example of accessing game code.
         LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
         {
-            if (loadScene != OWScene.SolarSystem) return;
-            ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            _currentScene = loadScene;
+            ResetHunger();
         };
     }
+
+    private void Update()
+    {
+        if (OWInput.IsNewlyPressed(InputLibrary.rollMode))
+        {
+            ResetHunger();
+            _hungerTimeLeft = 60f;
+        }
+        
+        if (_death || _currentScene != OWScene.SolarSystem)
+            return;
+        
+        _hungerTimeLeft -= Time.unscaledDeltaTime * Time.timeScale;  // Note: I have some doubts on the scalability of the Time.deltaTime variable.
+
+        if (!_criticalThresholdMet && _hungerTimeLeft <= CRITICAL_THRESHOLD)
+        {
+            _criticalThresholdMet = true;
+            NotificationManager.SharedInstance.PostNotification(
+                new NotificationData(
+                    NotificationTarget.Player,
+                    "60 SECONDS UNTIL STARVATION",
+                    5f,
+                    false));
+            Locator.GetPlayerAudioController().PlaySuitWarning();
+        }
+
+        if (_hungerTimeLeft <= 0f)
+        {
+            _death = true;
+            Locator.GetDeathManager().KillPlayer(DeathType.Asphyxiation);
+        }
+    }
+
+    public void ResetHunger()
+    {
+        _hungerTimeLeft = HUNGER_TIME_START;
+        _criticalThresholdMet = false;
+        _death = false;
+    }
+
+    public static void Log(string msg, MessageType type) => Instance.ModHelper.Console.WriteLine(msg, type);
 }
---
+++ DontStarveMod/ModPatches.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using OWML.Common;
+
+namespace DontStarveMod;
+
+[HarmonyPatch]
+public class ModPatches
+{
+    [HarmonyPatch(typeof(Marshmallow), nameof(Marshmallow.Eat))]
+    [HarmonyPostfix]
+    public static void Marshmallow_Eat_Postfix()
+    {
+        DontStarveMod.Log("Eating Marshmallow", MessageType.Success);
+        DontStarveMod.Instance.ResetHunger();
+    }
+}
